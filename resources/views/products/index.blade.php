5f187aa55df8f1cf4f75b9d401c3fb1728a79a91
Louille98 <louislemoine92@gmail.com>
2017-10-18 10:16:45 +0200

    Adding Products Functionality

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,18 +3,20 @@
 @section('content')
 
 <div class="container">
-  <div class="row">
-    <div class="col-md-8 col-md-offset-2">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          Products
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Products
+                </div>
+                <div class="panel-body">
+                    @foreach ($products as $product)
+
+                    @endforeach
+                </div>
+            </div>
         </div>
-        <div class="panel-body">
-          products...
-        </div>
-      </div>
     </div>
-  </div>
 </div>
 
 @endsection
